ee79ab482bc8caaace756ebabe5c43bba6ab0b5c
devmev10 <ravitokas90@gmail.com>
2022-12-02 11:38:22 +0530

    Add comments for better understanding

--- main.js
+++ main.js
@@ -5,31 +5,36 @@ document.querySelector('button').addEventListener('click', findMovie)
 
 function findMovie(){
     let title = document.querySelector('input').value
-    console.log(title)
+    
     fetch(`http://www.omdbapi.com/?s=${title}&apikey=23eb338b`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            
             let arr = data.Search.splice(0,3)
             
-            
+            // Loop over the array of 3 movies and extract movie details
             for(item of arr){
                 fetch(`http://www.omdbapi.com/?i=${item.imdbID}&apikey=23eb338b`)
                 .then(response => response.json())
                 .then(DATA => {
-                    console.log(DATA)
-                    localStorage.setItem('genre', DATA.imdbID)
-                    document.querySelector('main').innerHTML += `
-                                                            <img src = ${DATA.Poster}>
-                                                            <h2>${DATA.Title}</h2>
-                                                            <span>${DATA.imdbRating}</span>
-                                                            <span>${DATA.Runtime}</span>
-                                                            <span>${DATA.Genre}</span>
-                                                            <span>${DATA.Plot}</span>
-                                                            <span>${DATA.Year}</span>
-                                                            
-                                                            `
+                    
+                    let htmlContent = `<img src = ${DATA.Poster}>
+                    <h2>${DATA.Title}</h2>
+                    <span>${DATA.imdbRating}</span>
+                    <span>${DATA.Runtime}</span>
+                    <span>${DATA.Genre}</span>
+                    <span>${DATA.Plot}</span>
+                    <span>${DATA.Year}</span>`
+                    
+                    //Render movie details into HTML
+                    document.querySelector('main').innerHTML += htmlContent
+
+                    
                 })
+
+                //Local Storage for movies which are added via clicking watchlist button
+                
+            
                 
                 
             }
